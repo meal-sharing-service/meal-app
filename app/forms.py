b2bb63ce031cc9ea1968f71710b403c64f167fd4
Mark Swaringen <mswaringen@gmail.com>
2021-02-28 16:27:34 +0800

    create-offer-page

--- app/forms.py
+++ app/forms.py
@@ -14,7 +14,23 @@ class OfferForm(FlaskForm):
     expiration = StringField('Expiration', validators=[DataRequired()])
     category_id = IntegerField('Category ID (number)')
     condition = StringField('Condition', validators=[DataRequired()])
-    request = BooleanField('This is a request for food')
+    submit = SubmitField('Submit')
+
+class RequestForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    body = TextAreaField('Description', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    servings = IntegerField('Servings Offered', validators=[DataRequired()])
+    expiration = StringField('Expiration', validators=[DataRequired()])
+    category_id = IntegerField('Category ID (number)')
+    condition = StringField('Condition', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class EditOfferForm(FlaskForm):
+    body = TextAreaField('Description', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    category_id = IntegerField('Category ID (number)')
+    condition = StringField('Condition', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 class LoginForm(FlaskForm):
