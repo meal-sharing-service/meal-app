1f8a8c206a165a761440b4582ec43995582168a0
a01207202 <a01207202@unet.univie.ac.at>
2021-03-09 06:08:15 +0100

    testing spoonacular with model changes

--- app/__init__.py
+++ app/__init__.py
@@ -55,8 +55,7 @@ if not app.debug and not app.testing:
     else:
         if not os.path.exists('logs'):
             os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240,
-                                        backupCount=10)
+        file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=10)
         file_handler.setFormatter(logging.Formatter(
             '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
         file_handler.setLevel(logging.INFO)
