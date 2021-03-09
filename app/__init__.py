51af30b6f8fea2bd2787127314e4c3e8fa724e9c
a01207202 <a01207202@unet.univie.ac.at>
2021-03-09 07:06:25 +0100

    testing spoonacular with model changes

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ from flask_googlemaps import GoogleMaps
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
