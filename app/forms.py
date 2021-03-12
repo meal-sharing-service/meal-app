d6c33825d0f1240cd2b4b53470a2e5ad2846c07f
a01207202 <a01207202@unet.univie.ac.at>
2021-03-12 12:02:52 +0100

    map fix

--- app/forms.py
+++ app/forms.py
@@ -3,13 +3,16 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextA
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
 
+
 class MessageForm(FlaskForm):
     message = TextAreaField('Message', validators=[
         DataRequired(), Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
+
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
+
 
 class OfferForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
@@ -28,22 +31,17 @@ class OfferForm(FlaskForm):
     glutenFree = BooleanField('gluten free')
     submit = SubmitField('Submit')
 
+
 class OfferInfoForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
     body = TextAreaField('Description', validators=[
         DataRequired(), Length(min=1, max=140)])
-    pickup = TextAreaField('Pickup Instructions', validators=[
-        DataRequired(), Length(min=1, max=140)])
-    servings = IntegerField('Servings Offered', validators=[DataRequired()])
-    expiration = StringField('Expiration', validators=[DataRequired()])
-    category_id = IntegerField('Category ID (number)')
-    condition = StringField('Condition', validators=[DataRequired()])
     image = FileField('Photo of Food')
     vegan = BooleanField('vegan')
     vegetarian = BooleanField('vegetarian')
     dairyFree = BooleanField('dairy free')
     glutenFree = BooleanField('gluten free')
     submit = SubmitField('Submit')
+
 
 class RequestForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
@@ -57,6 +55,7 @@ class RequestForm(FlaskForm):
     condition = StringField('Condition', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
 class EditOfferForm(FlaskForm):
     body = TextAreaField('Description', validators=[
         DataRequired(), Length(min=1, max=140)])
@@ -66,22 +65,26 @@ class EditOfferForm(FlaskForm):
     condition = StringField('Condition', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
+
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
+
 
 class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Request Password Reset')
-    
+
+
 class RegistrationForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
@@ -106,6 +109,7 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
