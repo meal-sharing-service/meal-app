fdf7fd413d2f2ce18c6131bcf60fc721bb917fa1
a01207202 <a01207202@unet.univie.ac.at>
2021-03-17 13:32:32 +0100

    added Spoonacular stuff

--- app/routes.py
+++ app/routes.py
@@ -79,6 +79,7 @@ def create_offer():
         flash('Please add some more information!')
 
         session['offer_data'] = data
+        print(session)
         return redirect(url_for('add_offer_info'))
     return render_template('create_offer.html', title='Share Food', form=form)
 
